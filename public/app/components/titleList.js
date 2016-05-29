cefbdaed71adce11c76139774b31c7b741acfef5
Ahad Wasim <ahadwasim@gmail.com>
2016-05-29 16:50:23 -0700

    add titlelist component

---
+++ public/app/components/titleList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Title from './title.js'
+
+const TitleList = (props) => {
+
+  var renderedReportLists = props.reportList.map((report)=> {
+
+  });
+
+  return (
+    <ul>
+      { reportrenderedReportListsList }
+    </ul>
+  );
+}
+
+export default TitleList;
